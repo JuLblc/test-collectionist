d6a1148d65eae5354b254c516a92039dc3be7708
Julien <julien.leblanc@epfedu.fr>
2024-01-09 20:16:37 +0100

    add tailwind to main.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      lineClamp: {
-        8:"8"
-      },
-      spacing: {
-        '108': '27rem',
-      },
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    lineClamp: {
+      8: "8"
+    },
+    spacing: {
+      '108': '27rem',
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
